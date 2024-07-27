19d6fbb7735370b13719527881f9d6722b083294
Vishesh Gupta <vishesh.gupta@sprinklr.com>
2024-07-28 00:08:11 +0530

    setup init

---
+++ src/main/java/com/ticket/booking/platform_backend/PlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.booking.platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformBackendApplication.class, args);
+	}
+
+}
